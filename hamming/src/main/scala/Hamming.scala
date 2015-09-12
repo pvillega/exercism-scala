af747d56921476ac8c8bf01f3ce3544ee8c59884
Pere Villega <pere.villega@barclayscapital.com>
2015-09-12 12:35:02 +0100

    Third exercise

--- hamming/src/main/scala/Hamming.scala
+++ hamming/src/main/scala/Hamming.scala
@@ -1,6 +1,17 @@
-/**
- * Created by villegap on 12/09/2015.
- */
 object Hamming {
 
+  private def calculateHammingDifference(source: String, other: String): Int = {
+    def areCharsEqual(a: Char, b: Char): Int = if(a == b) 0 else 1
+
+    @scala.annotation.tailrec
+    def tailRecCalculation(sa: String, sb: String, acc: Int): Int =
+      if(sa.length == 0) acc
+      else tailRecCalculation(sa.tail, sb.tail, acc + areCharsEqual(sa.head, sb.head))
+
+    tailRecCalculation(source, other, 0)
+  }
+
+  def compute(seqA: String, seqB: String) =
+    if(seqA.length != seqB.length) throw new IllegalArgumentException("Strands of different length")
+    else calculateHammingDifference(seqA, seqB)
 }
